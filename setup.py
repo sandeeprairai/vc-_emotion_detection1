d98e87e24a1bf956b65a9b151ad9e2986b01f1f1
sandeeprairai <sandeeprairai339@gmail.com>
2024-07-22 17:00:18 +0530

    add cookicutter to the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a project to predict emotion of the human',
+    author='sandeep kumar',
+    license='',
+)
